a72e2914e0487ab59c58360399f106fb6840290f
Ahmed Elkordy <akelkordy@ucsb.edu>
2022-10-03 09:24:52 -0700

    Renamed ot_run.py to volt.py and fixed affected files

--- examples/run_ted_bilingual.sh
+++
@@ -1,4 +0,0 @@
-bash prepare-ted-bilingual.sh tedbpe30000 30000 ted/data #$1: output file, $2: bpe-size (here we adopt a very large one)
-bash prepare-ted-bilingual-volt.sh tedbilingualvolt tedbpe30000 ted/data
-
-
